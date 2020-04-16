1032be03168404c11a348d27c105f680a32abc57
Eli Tyre <elityre@gmail.com>
2020-04-16 09:11:02 -0700

    still has some bugs

--- simple_GUI_stopwatch_converted_to_timer_messing_around.py
+++ simple_GUI_stopwatch_converted_to_timer_messing_around.py
@@ -4,10 +4,26 @@
 import Tkinter as Tkinter 
 # note to future self: Tkinter is capitalized for python 2, but not for python 3.
   
-counterR = 20
+counterR = 3900
 counterL = 20
+# the counters are both in the units of "seconds"
 running = False
-rocker = "R"
+rocker = "L"
+
+def make_string_and_show_two_digits(number):
+    if len(str(number)) >= 2:
+        return str(number)
+    else:
+        return "0"+str(number)
+
+
+def convert_seconds_to_time_format(total_seconds):
+    hours = total_seconds / 3600
+    minutes = (total_seconds%3600) / 60
+    seconds = (total_seconds%3600)%60
+    full_timer = make_string_and_show_two_digits(hours)+":"+make_string_and_show_two_digits(minutes)+":"+make_string_and_show_two_digits(seconds)
+    return full_timer
+
 def counter_label(label): 
     def count(): 
         if running: 
@@ -24,7 +40,7 @@ def counter_label(label):
             if counterR <= 0 and counterL <= 0:
                 rocker = 0
 
-            display=str(counterR)+"  "+str(counterL) 
+            display=convert_seconds_to_time_format(counterL)+"  "+convert_seconds_to_time_format(counterR) 
   
             label['text']=display   # Or label.config(text=display) 
   
@@ -59,8 +75,17 @@ def Stop():
     stop['state']='disabled'
     reset['state']='normal'
     running = False
-  
-# Reset function of the stopwatch 
+
+# Switch the rocker
+def Switch(label): 
+    global rocker
+    if rocker == "R":
+        rocker = "L"
+    if rocker == "L":
+        rocker = "R"
+    reset['state']='normal'
+
+# Reset function of the timer
 def Reset(label): 
     global counter 
     counter=-1
@@ -78,16 +103,26 @@ root = Tkinter.Tk()
 root.title("Stopwatch") 
   
 # Fixing the window size. 
-root.minsize(width=200, height=130) 
+root.minsize(width=200, height=140) 
 label = Tkinter.Label(root, text="Welcome!", fg="black", font="Verdana 30 bold") 
 label.pack() 
+
+#this text defines the buttons that call the functions
 start = Tkinter.Button(root, text='Start',  
-width=15, command=lambda:Start(label)) 
+width=15, command=lambda:Start(label))
+
 stop = Tkinter.Button(root, text='Stop',  
 width=15, state='disabled', command=Stop) 
+
+switch = Tkinter.Button(root, text='Switch',  
+width=15, command=lambda:Switch(label))
+
 reset = Tkinter.Button(root, text='Reset', 
  width=15, state='disabled', command=lambda:Reset(label)) 
+
+#this text puts the button in the GUI
 start.pack() 
-stop.pack() 
+stop.pack()
+switch.pack() 
 reset.pack() 
 root.mainloop() 
